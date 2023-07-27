6394daad1c87318abf0a470e2fe87e6b666da89c
Wastard <Ilya.pakylov@gmail.com>
2023-07-27 12:51:07 +0300

    Initial Vite commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Header from './components/Header/Header'
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <Header/>
+      </div>
+    </>
+  )
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import css from './Header.module.css'
+
+export default function Header() {
+  return (
+    <div className={css.headerContainer}>
+
+    </div>
+  )
+}
+
