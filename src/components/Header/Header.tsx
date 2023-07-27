e91aadc41a1923ee83c043ad3e9708dacdccba42
Wastard <Ilya.pakylov@gmail.com>
2023-07-27 16:21:37 +0300

    added shopping cart, location and logo at header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,20 @@
 import css from './Header.module.css';
+import 'typeface-inter';
+import { ReactSVG } from 'react-svg';
 
 export default function Header() {
-  return <div className={css.headerContainer}></div>;
+  return (
+    <div className={css.headerContainer}>
+      <div className={css.locationContainer}>
+        <ReactSVG id={css.locationPoint} src='icons/location.svg' />
+        <p>Санкт-Петербург</p>
+      </div>
+      <div className={css.iconContainer}>
+        <ReactSVG id={css.icon} src='icons/fullicon.svg'></ReactSVG>
+      </div>
+      <div className={css.shoppingCartContainer}>
+        <ReactSVG id={css.shoppingCart} src='icons/shoppingcart.svg' />
+      </div>
+    </div>
+  );
 }
