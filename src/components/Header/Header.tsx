01ab0c6fba5ccd2dd4b9128b740d5d486a5ef91c
Wastard <Ilya.pakylov@gmail.com>
2023-07-27 20:12:59 +0300

    added location wrapper; added select location function

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
             <ShoppingCart />
           </div>
         </div>
-        <div className={css.iconInnerContainer}></div>
+        {/* <div className={css.iconInnerContainer}></div> */}
       </div>
     </>
   );
--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -1,10 +1,42 @@
+import { useState } from 'react';
 import { ReactSVG } from 'react-svg';
 import css from './Location.module.css';
+import { locations } from '../../utils/data';
+
 export default function Location() {
+  const [currentCity, setCurrentCity] = useState(locations[0]);
+  const [isHide, setIsHide] = useState(true);
+
   return (
-    <div className={css.locationContainer}>
-      <ReactSVG id={css.locationPoint} src='icons/location.svg' />
-      <p>Санкт-Петербург</p>
-    </div>
+    <>
+      <div
+        className={css.locationContainer}
+        onMouseEnter={() => setIsHide(false)}
+      >
+        <ReactSVG id={css.locationPoint} src='icons/location.svg' />
+        <p>{currentCity}</p>
+
+        {isHide || (
+          <div className={css.wrapper} onMouseLeave={() => setIsHide(true)}>
+            {locations.map((el: string, i: number) => {
+              if (el === currentCity) return null;
+              else
+                return (
+                  <div
+                    key={i}
+                    id={css.locationElementContainer}
+                    onClick={() => {
+                      setIsHide(true);
+                      setCurrentCity(locations[i]);
+                    }}
+                  >
+                    <p id={css.locationElementText}>{el}</p>
+                  </div>
+                );
+            })}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
