5a5c4e5b039c0d2c843262bbf1bb3a6a0a13ba09
Wastard <Ilya.pakylov@gmail.com>
2023-07-28 11:51:39 +0300

    updated settings LocationWrapper

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,23 @@
 import css from './Header.module.css';
 import 'typeface-inter';
+import { useContext } from 'react';
+import { LocationContext } from '../../providers/LocationProvider';
 import { ReactSVG } from 'react-svg';
 import ShoppingCart from '../ShoppingCart/ShoppingCart';
 import Location from '../Location/Location';
 
 export default function Header() {
+  const { setIsHide } = useContext(LocationContext);
+
   return (
     <>
       {/* <ReactSVG id={css.icon} src='icons/fullicon.svg'></ReactSVG> */}
       <div className={css.iconContainer}>
         <div className={css.headerContainer}>
-          <div className={css.headerInnerContainer}>
+          <div
+            className={css.headerInnerContainer}
+            onMouseLeave={() => setIsHide(true)}
+          >
             <Location />
             <ShoppingCart />
           </div>
