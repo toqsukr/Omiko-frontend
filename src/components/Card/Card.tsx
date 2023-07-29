82472eb0b01085107e9bfbea2f912e06f0a9224d
Wastard <Ilya.pakylov@gmail.com>
2023-07-29 20:04:10 +0300

    updated card hover style

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -14,7 +14,11 @@ export default function Card({ ...props }: ICard) {
       </div>
       <div className={css.cardInfo}>
         <p id={css.price}>{props.price}</p>
-        <p id={css.title}>{`${props.title.slice(0, 27)}...`}</p>
+        <p id={css.title}>
+          {props.title.length > 28
+            ? `${props.title.slice(0, 27)}...`
+            : props.title}
+        </p>
       </div>
       <div className={css.toCartContainer}>
         <a type='button' className={css.toCartButton}>
