5e8fa9144e7390d687874312f5f5d05b0345c210
Wastard <Ilya.pakylov@gmail.com>
2023-07-30 05:42:15 +0300

    added card loading without some data

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,23 +1,25 @@
 import css from './Card.module.css';
 
 interface ICard {
-  price: string;
-  title: string;
-  url: string;
+  price?: string;
+  title?: string;
+  url?: string;
 }
 
 export default function Card({ ...props }: ICard) {
   return (
     <div className={css.cardContainer}>
       <div className={css.imageContainer}>
-        <img id={css.image} src={props.url} alt='' />
+        <img id={css.image} src={props.url ? props.url : ''} alt='' />
       </div>
       <div className={css.cardInfo}>
-        <p id={css.price}>{props.price}</p>
+        <p id={css.price}>{props.price ? props.price : 'Цена'}</p>
         <p id={css.title}>
-          {props.title.length > 28
-            ? `${props.title.slice(0, 27)}...`
-            : props.title}
+          {props.title
+            ? props.title.length > 28
+              ? `${props.title.slice(0, 27)}...`
+              : props.title
+            : 'Название'}
         </p>
       </div>
       <div className={css.toCartContainer}>
