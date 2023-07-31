ea594ec7ac772fc225e4446844ea153d5a585652
Wastard <Ilya.pakylov@gmail.com>
2023-07-31 19:23:47 +0300

    added delivery details to delivery section

--- src/components/DeliverySection/DeliverySection.tsx
+++ src/components/DeliverySection/DeliverySection.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import css from './DeliverySection.module.css';
+import { deliveryInfo } from '../../utils/data';
 import { LocationContext } from '../../providers/LocationProvider';
 
 export default function DeliverySection() {
@@ -8,12 +9,20 @@ export default function DeliverySection() {
     <div className={css.sectionContainer}>
       <div className={css.titleContainer}>
         <h1 id={css.title}>Доставка</h1>
-        <p id={css.city}>{`г. ${location.city}`}</p>
+        <p id={css.city}>
+          {location.city === 'Другой'
+            ? 'в регионы России'
+            : `г. ${location.city}`}
+        </p>
       </div>
       <div className={css.sectionInnerContainer}>
         <div className={css.info}>
-          <div className={css.infoText}></div>
-          <div className={css.infoButton}></div>
+          <div className={css.infoTextContainer}>
+            <p id={css.infoText}>{deliveryInfo}</p>
+          </div>
+          <div className={css.infoButton}>
+            <p id={css.infoButtonText}>детали доставки</p>
+          </div>
         </div>
         <div className={css.convertor}>
           <div className={css.convertorData}>
