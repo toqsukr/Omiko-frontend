5c308267375416471f952a787e83367206e559f6
Wastard <Ilya.pakylov@gmail.com>
2023-08-01 10:27:50 +0300

    updated delivery details window design, added closing it

--- src/components/DeliveryDetail/DeliveryDetail.tsx
+++ src/components/DeliveryDetail/DeliveryDetail.tsx
@@ -1,13 +1,18 @@
 import css from './DeliveryDetail.module.css';
+import { deliveryDetails } from '../../utils/data';
 
 export default function DeliveryDetail() {
   return (
-    <>
+    <div className={css.sideInnerContainer}>
       <div className={css.titleSideContainer}>
-        <h1 id={css.titleText}></h1>
+        <h1 id={css.titleText}>{deliveryDetails.title}</h1>
       </div>
-      <div className={css.mainInfoContainer}></div>
-      <div className={css.otherInfoContainer}></div>
-    </>
+      <div className={css.mainInfoContainer}>
+        <p id={css.mainInfoText}>{deliveryDetails.mainInfo}</p>
+      </div>
+      <div className={css.otherInfoContainer}>
+        <p id={css.otherInfoText}>{deliveryDetails.otherInfo}</p>
+      </div>
+    </div>
   );
 }
--- src/components/PopupWindow/PopupWindow.tsx
+++ src/components/PopupWindow/PopupWindow.tsx
@@ -1,10 +1,31 @@
 import css from './PopupWindow.module.css';
+import classnames from 'classnames';
+import { ReactSVG } from 'react-svg';
 import DeliveryDetail from '../DeliveryDetail/DeliveryDetail';
 
-export default function PopupWindow() {
+interface IPopupWindow {
+  isShow: boolean;
+  setShow: (value: boolean) => void;
+}
+
+export default function PopupWindow({ isShow, setShow }: IPopupWindow) {
   return (
-    <div className={css.mainSideContainer}>
-      <DeliveryDetail />
-    </div>
+    <>
+      <div
+        className={classnames({
+          [css.mainSideContainer]: true,
+          [css.locked]: isShow,
+        })}
+        onClick={() => setShow(false)}
+      ></div>
+      <div className={css.mainSideWindow}>
+        <DeliveryDetail />
+        <ReactSVG
+          id={css.close}
+          src='icons/close.svg'
+          onClick={() => setShow(false)}
+        />
+      </div>
+    </>
   );
 }
