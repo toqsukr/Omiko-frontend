41bb5a15e8333437e34112cd8d5acc07635596a8
Wastard <Ilya.pakylov@gmail.com>
2023-09-11 11:53:27 +0300

    set adapted design using react-responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,37 @@
 import AdvantageSection from '@components/advantageSection/advantageSection.component';
 import MobileBar from '@components/bar/mobileBar/MobileBar.component';
+import HeaderDesktop from '@components/header/headerDesktop/headerDesktop.component';
 import HeaderMobile from '@components/header/headerMobile/headerMobile.component';
-import PopularProducts from '@components/popularProducts/popularProducts.component';
 import SectionContainer from '@components/sectionContainer/sectionContainer.component';
 import LocationProvider from '@providers/LocationProvider';
 import RootShowProvider from '@providers/showProviders/unitProviders/RootShowProvider';
 import { FC } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { Element } from 'react-scroll';
 import './App.css';
 
 const App: FC = () => {
+  const isMobile = useMediaQuery({ maxDeviceWidth: 500 });
   return (
     <RootShowProvider>
       <LocationProvider>
-        <HeaderMobile />
+        {isMobile ? <HeaderMobile /> : <HeaderDesktop />}
         <div id="bodyContainer">
           <Element name="top">
             <SectionContainer />
           </Element>
-          <PopularProducts />
-          {/* <Element name="delivery">
+          {/* <PopularProducts />
+          <Element name="delivery">
             <DeliverySection />
           </Element> */}
           <Element name="advantage">
             <AdvantageSection />
           </Element>
-          {/* 
-          <Element name="about">
+          {/* <Element name="about">
             <AboutSection />
-          </Element>
-          <Footer />  */}
-          <MobileBar />
+          </Element> */}
+          {/* <Footer /> */}
+          {isMobile && <MobileBar />}
         </div>
       </LocationProvider>
     </RootShowProvider>
