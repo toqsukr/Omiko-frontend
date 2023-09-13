cb023a381dd63ac69634e1bb3aca2143386111c5
Wastard <Ilya.pakylov@gmail.com>
2023-09-13 22:00:12 +0300

    hide productBy component at mobile + very smol

--- src/components/Footer/footer.component.tsx
+++ src/components/Footer/footer.component.tsx
@@ -1,17 +1,20 @@
 import { FC } from 'react';
+import { useMediaQuery } from 'react-responsive';
+
 import { footer } from './footer.data';
 
 import css from './Footer.module.css';
 import ProductBy from './productBy/productBy.component';
 
 const Footer: FC = () => {
+  const isMobile = useMediaQuery({ maxDeviceWidth: 500 });
   return (
     <footer id={css.footerContainer}>
       <div id={css.contentContainer}>
         <div id={css.descriptionContainer}>
           <p className={css.footerText}>{footer.description}</p>
         </div>
-        <ProductBy />
+        {isMobile || <ProductBy />}
       </div>
     </footer>
   );
