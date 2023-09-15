bfab47e5ae11a006980803494878b84a15ac8ac0
Wastard <Ilya.pakylov@gmail.com>
2023-09-15 23:04:04 +0300

    fixed horizontal scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import AboutSection from '@components/aboutSection/aboutSection.component';
 import AdvantageSection from '@components/advantageSection/advantageSection.component';
 import MobileBar from '@components/bar/mobileBar/MobileBar.component';
+import DeliverySection from '@components/deliverySection/deliverySection.component';
 import Footer from '@components/footer/footer.component';
 import HeaderDesktop from '@components/header/headerDesktop/headerDesktop.component';
 import HeaderMobile from '@components/header/headerMobile/headerMobile.component';
+import PopularProducts from '@components/popularProducts/popularProducts.component';
 import SectionContainer from '@components/sectionContainer/sectionContainer.component';
-import LocationProvider from '@providers/LocationProvider';
 import RootShowProvider from '@providers/showProviders/unitProviders/RootShowProvider';
 import { FC } from 'react';
 import { useMediaQuery } from 'react-responsive';
@@ -16,26 +17,24 @@ const App: FC = () => {
   const isMobile = useMediaQuery({ maxDeviceWidth: 500 });
   return (
     <RootShowProvider>
-      <LocationProvider>
-        {isMobile ? <HeaderMobile /> : <HeaderDesktop />}
-        <div id="bodyContainer">
-          <Element name="top">
-            <SectionContainer />
-          </Element>
-          {/* <PopularProducts />
-          <Element name="delivery">
-            <DeliverySection />
-          </Element> */}
-          <Element name="advantage">
-            <AdvantageSection />
-          </Element>
-          <Element name="about">
-            <AboutSection />
-          </Element>
-          <Footer />
-          {isMobile && <MobileBar />}
-        </div>
-      </LocationProvider>
+      {isMobile ? <HeaderMobile /> : <HeaderDesktop />}
+      <div id="bodyContainer">
+        <Element name="top">
+          <SectionContainer />
+        </Element>
+        <PopularProducts />
+        <Element name="delivery">
+          <DeliverySection />
+        </Element>
+        <Element name="advantage">
+          <AdvantageSection />
+        </Element>
+        <Element name="about">
+          <AboutSection />
+        </Element>
+        <Footer />
+        {isMobile && <MobileBar />}
+      </div>
     </RootShowProvider>
   );
 };
